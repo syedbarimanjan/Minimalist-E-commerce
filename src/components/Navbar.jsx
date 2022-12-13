9f5d9c783385cd4a0737f37f6e1ab8eed44fc1b6
unknown <lol.delta123@gmail.com>
2022-12-13 17:30:54 +0100

    navbar change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
 import "./Navbar.css";
 import LogoImg from "../img/logo.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 10) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
   return (
     <>
       <nav className="navbar">
         <div className="container">
-          <div className="nav-container">
+          <div className={`nav-container ${sticky ? "cont-sticky" : ""}`}>
             <Link to="/">
               <img src={LogoImg} alt="logo" className="logo-img" />
             </Link>
